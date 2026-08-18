df71048eb7dbe74f089031f896ce475fe230bb66
daanniill <daniil.malakhov12@gmail.com>
2026-08-17 17:57:40 -0700

    created order routes and added routing to main

--- application/routes.go
+++ application/routes.go
@@ -3,6 +3,7 @@ package application
 import (
 	"net/http"
 
+	"github.com/daanniill/Jenova_Test_Environment/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -16,5 +17,17 @@ func loadRoutes() *chi.Mux {
 		w.WriteHeader(http.StatusOK)
 	})
 
+	router.Route("/orders", loadOrderRoutes)
+
 	return router
+}
+
+func loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{}
+
+	router.Post("/", orderHandler.Create)
+	router.Get("/", orderHandler.List)
+	router.Get("/{id}", orderHandler.GetByID)
+	router.Put("/{id}", orderHandler.UpdateByID)
+	router.Delete("/", orderHandler.DeleteByID)
 }
